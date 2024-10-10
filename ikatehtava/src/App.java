49dc2e4c7148e7942637184644195671501cfcdc
Rawerzz <jukka.j.ketonen@gmail.com>
2024-10-10 17:29:36 +0300

    Add files via upload

---
+++ ikatehtava/src/App.java
@@ -0,0 +1,62 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        int ika = 10;
+
+// Tulostusehdot
+
+
+if (ika > 0 && ika < 15) {
+    System.out.println("Olet alaikäinen");
+    if (ika >= 15) {
+    System.out.println("Saat ajaa mopoa"); }
+    } else if (ika >= 65) {
+    System.out.println("Olet eläkeläinen");
+
+    } 
+if (ika == 16 || ika == 17)
+System.out.println("Saat ajaa kevaria");
+else if (ika < 16)
+System.out.println("Olet alle 16 et saa ajaa mopoa"); 
+else if (ika > 18 && ika >= 65)
+System.out.println("Olet eläkeläinen");
+{
+}      
+if (ika == 18)
+System.out.println("Olet juuri tullut täysi-ikäiseksi ja saat ajaa autoa"); 
+else if (ika < 18)
+System.out.println("Olet alle 18 et saa ajaa autoa"); 
+ else if (ika >= 65) {
+    System.out.println("Olet eläkeläinen");
+    
+    {
+    if (ika == 10 || ika == 20 || ika == 30 || ika == 40 || ika == 50 || ika == 60 ||
+    ika ==  70 || ika == 80 || ika ==  90)
+     System.out.println("Onneksi olkoon");
+    }{
+    if (ika == 100) 
+    System.out.println("Paljon");
+    if (ika == 100) 
+    System.out.println("Onnea");
+     if (ika == 100) 
+    System.out.println("100 Vuotias!");
+    }
+    if (ika == 58) 
+    System.out.println("Voit mennä varhais eläkkeelle");{
+        
+    }
+    if (ika == 65 ) 
+    System.out.println("Hyviä eläkepäiviä!");{
+        
+    }
+    if (ika >= 40 && ika <= 50) 
+    System.out.println("Parasta keski-ikää!");{
+        
+    }
+    }
+    }
+}
+
+
+
